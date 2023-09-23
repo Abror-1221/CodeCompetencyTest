d03dd3c2627423def954d6eed994d1f740812242
Mulia Abror Khairul <muliausman12@gmail.com>
2023-09-23 15:19:05 +0700

    Initial commit

---
+++ RuleLogicNumber/NumberLogicRules.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleLogicNumber
+{
+    class NumberLogicRules
+    {
+        private Dictionary<int, string> rules = new Dictionary<int, string>();
+        private List<int> ruleOrder = new List<int>();
+
+        public void AddRule(int input, string output)
+        {
+            rules[input] = output;
+            if (!ruleOrder.Contains(input))
+            {
+                ruleOrder.Add(input);
+                ruleOrder.Sort();
+            }
+        }
+
+        public void EditRule(int input, string newOutput)
+        {
+            if (rules.ContainsKey(input))
+            {
+                rules[input] = newOutput;
+            }
+            else
+            {
+                Console.WriteLine($"Rule with input {input} does not exist.");
+            }
+        }
+
+        public void DeleteRule(int input)
+        {
+            if (rules.ContainsKey(input))
+            {
+                rules.Remove(input);
+                ruleOrder.Remove(input);
+            }
+            else
+            {
+                Console.WriteLine($"Rule with input {input} does not exist.");
+            }
+        }
+
+        public void ShowRules()
+        {
+            foreach (var input in ruleOrder)
+            {
+                Console.WriteLine($"Input: {input}, Output: {rules[input]}");
+            }
+        }
+
+        public void Generate(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                string result = "";
+
+                foreach (var input in ruleOrder)
+                {
+                    if (i % input == 0)
+                    {
+                        result += rules[input];
+                    }
+                }
+
+                if (string.IsNullOrEmpty(result))
+                {
+                    Console.Write(i);
+                }
+                else
+                {
+                    Console.Write(result);
+                }
+
+                if (i != n)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+    }
+}
---
+++ RuleLogicNumber/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuleLogicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberLogicRules myClass = new NumberLogicRules();
+
+            Console.WriteLine("Console app is running.");
+
+            while (true)
+            {
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("1. Add Rule");
+                Console.WriteLine("2. Edit Rule");
+                Console.WriteLine("3. Delete Rule");
+                Console.WriteLine("4. Show Rules");
+                Console.WriteLine("5. Generate");
+                Console.WriteLine("6. Quit");
+                Console.Write("Enter your choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter input for the new rule: ");
+                        int input = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter output for the new rule: ");
+                        string output = Console.ReadLine();
+                        myClass.AddRule(input, output);
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Write("Enter the input of the rule you want to edit: ");
+                        int editInput = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the new output: ");
+                        string newOutput = Console.ReadLine();
+                        myClass.EditRule(editInput, newOutput);
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Write("Enter the input of the rule you want to delete: ");
+                        int deleteInput = Convert.ToInt32(Console.ReadLine());
+                        myClass.DeleteRule(deleteInput);
+                        Console.Clear();
+                        break;
+                    case 4:
+                        myClass.ShowRules();
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Console.Write("Enter a value for n: ");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        myClass.Generate(n);
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 6:
+                        return; // Exit the program
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
+
